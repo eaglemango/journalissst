4e5e0143d7ced9f24aa083a3022923aa9dddf25d
Sergey Gorbunov <eaglemango@gmail.com>
2019-04-23 14:24:10 +0300

    Change files structure

---
+++ utils/news_parser.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+from bs4 import BeautifulSoup
+import pickle
+import requests
+import time
+
+HEADERS = {
+    "User-Agent": "Chrome/73.0.3683.103"
+    }
+
+
+class NewsParser(ABC):
+    @abstractmethod
+    def parse(self, page_count: int) -> None:
+        pass
+
+
+class IPhonesNewsParser(NewsParser):
+    def parse(self, page_count: int) -> None:
+        BASE_URL = "https://www.iphones.ru/page"
+
+        post_list = []
+        for page in range(1, page_count):
+            response = requests.get("{0}/{1}".format(BASE_URL, page), headers=HEADERS)
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            time.sleep(1)
+
+            post_list.extend(soup.find_all("h2", {"class": "postitem__content-h"}))
+
+        link_list = []
+        for post in post_list:
+            soup = BeautifulSoup(str(post), "html.parser")
+
+            link_list.append(soup.find("a").get("href"))
+
+        with open("./news_data/it", "rb") as it_dict_data:
+            it_dict = pickle.load(it_dict_data)
+
+        for link in link_list:
+            response = requests.get(link, headers=HEADERS)
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            for paragraph in soup.select(".post > p"):
+                words = paragraph.text.split()
+
+                for i in range(len(words) - 1):
+                    if words[i] not in it_dict:
+                        it_dict[words[i]] = {}
+
+                    if words[i + 1] not in it_dict[words[i]]:
+                        it_dict[words[i]][words[i + 1]] = 1
+                    else:
+                        it_dict[words[i]][words[i + 1]] += 1
+
+        with open("./news_data/it", "wb") as it_dict_data:
+            pickle.dump(it_dict, it_dict_data)
