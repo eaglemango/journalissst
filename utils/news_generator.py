ff95d9d30e9dbfaeb6d67c9826869778496eb5b0
Sergey Gorbunov <eaglemango@gmail.com>
2019-05-03 22:42:17 +0300

    Add 2-tier parser and generator

--- utils/news_generator.py
+++ utils/news_generator.py
@@ -43,12 +43,55 @@ class ITNewsGenerator(NewsGenerator):
         while word in self.dictionary:
             total_followers = sum(self.dictionary[word].values())
 
-            probabilities = [follower / total_followers for follower in self.dictionary[word].values()]
+            probabilities = [follower / total_followers
+                             for follower in
+                             self.dictionary[word].values()]
 
-            word = numpy.random.choice(list(self.dictionary[word]), p=probabilities)
+            word = numpy.random.choice(list(self.dictionary[word]),
+                                       p=probabilities)
+
             words.append(preprocess(word, next_title))
 
             next_title = is_next_title(word)
+
+        if len(words) < min_length:
+            return self.generate(min_length - 1)
+        else:
+            return ' '.join(words)
+
+
+class ImprovedITNewsGenerator(NewsGenerator):
+    def __init__(self) -> None:
+        super().__init__()
+
+        with open("./news_data/improved_it", "rb") as it_dict_data:
+            self.dictionary = pickle.load(it_dict_data)
+
+    def generate(self, min_length: int) -> str:
+        words = []
+
+        next_title = True
+
+        word_pair = numpy.random.choice(list(self.dictionary))
+        words.append(preprocess(word_pair, next_title))
+
+        next_title = is_next_title(word_pair)
+
+        while word_pair in self.dictionary:
+            total_followers = sum(self.dictionary[word_pair].values())
+
+            probabilities = [follower / total_followers
+                             for follower in
+                             self.dictionary[word_pair].values()]
+
+            next_word = numpy.random.choice(list(self.dictionary[word_pair]),
+                                            p=probabilities)
+
+            word_pair = "{0} {1}".format(word_pair.split(' ')[1], next_word)
+
+            words.append(preprocess(next_word, next_title))
+
+            next_title = is_next_title(word_pair)
 
         if len(words) < min_length:
             return self.generate(min_length - 1)
--- utils/news_parser.py
+++ utils/news_parser.py
@@ -55,3 +55,47 @@ class IPhonesNewsParser(NewsParser):
 
         with open("./news_data/it", "wb") as it_dict_data:
             pickle.dump(it_dict, it_dict_data)
+
+
+class ImprovedIPhonesNewsParser(NewsParser):
+    def parse(self, page_count: int) -> None:
+        BASE_URL = "https://www.iphones.ru/page"
+
+        post_list = []
+        for page in range(1, page_count):
+            response = requests.get("{0}/{1}".format(BASE_URL, page), headers=HEADERS)
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            time.sleep(1)
+
+            post_list.extend(soup.find_all("h2", {"class": "postitem__content-h"}))
+
+        link_list = []
+        for post in post_list:
+            soup = BeautifulSoup(str(post), "html.parser")
+
+            link_list.append(soup.find("a").get("href"))
+
+        with open("./news_data/improved_it", "rb") as it_dict_data:
+            it_dict = pickle.load(it_dict_data)
+
+        for link in link_list:
+            response = requests.get(link, headers=HEADERS)
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            for paragraph in soup.select(".post > p"):
+                words = paragraph.text.split()
+
+                for i in range(len(words) - 2):
+                    word_pair = "{0} {1}".format(words[i], words[i + 1])
+
+                    if word_pair not in it_dict:
+                        it_dict[word_pair] = {}
+
+                    if words[i + 2] not in it_dict[word_pair]:
+                        it_dict[word_pair][words[i + 2]] = 1
+                    else:
+                        it_dict[word_pair][words[i + 2]] += 1
+
+        with open("./news_data/improved_it", "wb") as it_dict_data:
+            pickle.dump(it_dict, it_dict_data)
